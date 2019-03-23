fe9756d19e0bf565171b89af2fcc8e043cdd1a19
Nick Dimitrov <snoop088@gmail.com>
2019-03-23 22:40:25 +0200

    AddItem implementation

---
+++ AddItem/index.js
@@ -0,0 +1,33 @@
+
+var MongoClient = require('mongodb').MongoClient;
+
+module.exports = async function (context, req) {
+    const response = (client, context) => (status, body) => {
+        context.res = {
+            status: status,
+            body: body
+        };
+        client.close();
+        context.done();
+    }
+    MongoClient.connect(process.env.CosmosDBConnectionString, (err, client) => {
+        let send = response(client, context);
+        if (err) send(500, err.message);
+        let db = client.db('dazzledb');
+        let item = ({ id, name, team, phone } = req.body);
+
+        db.collection('items').insertOne(
+            {
+                id: item.id,
+                name: item.name,
+                team: item.team,
+                phone: item.phone
+            },
+            (err, items) => {
+                if (err) send(500, err.message);
+                console.log('items', items);
+                send(200, item);
+            }
+        );
+    })
+};
