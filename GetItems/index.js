03a63a4bee323136ffc1dcda8716a74aa6ebd114
Nick Dimitrov <snoop088@gmail.com>
2019-04-04 23:14:43 +0300

    stipdown getitems

--- GetItems/index.js
+++ GetItems/index.js
@@ -5,27 +5,27 @@ var assert = require('assert');
 
 module.exports = function (context, req) {
     console.log(process.env.CosmosDBConnectionString);
-    MongoClient.connect(process.env.CosmosDBConnectionString, {useNewUrlParser: true}, (err, client) => {
-        assert.equal(null, err);
-        const response = (client, context) => (status, body) => {
-            context.res = {
-                status: status,
-                body: body
-            };
-            client.close();
-            context.done();
-        }
-        let send = response(client, context);
-        if (err) send(500, err.message);
+    // MongoClient.connect(process.env.CosmosDBConnectionString, {useNewUrlParser: true}, (err, client) => {
+    //     assert.equal(null, err);
+    //     const response = (client, context) => (status, body) => {
+    //         context.res = {
+    //             status: status,
+    //             body: body
+    //         };
+    //         client.close();
+    //         context.done();
+    //     }
+    //     let send = response(client, context);
+    //     if (err) send(500, err.message);
 
-        let db = client.db('dazzledb');
+    //     let db = client.db('dazzledb');
 
-        db
-            .collection('items')
-            .find({})
-            .toArray((err, result) => {
-                if (err) send(500, err.message);
-                send(200, JSON.parse(JSON.stringify(result)));
-            });
-    });
+    //     db
+    //         .collection('items')
+    //         .find({})
+    //         .toArray((err, result) => {
+    //             if (err) send(500, err.message);
+    //             send(200, JSON.parse(JSON.stringify(result)));
+    //         });
+    // });
 };
