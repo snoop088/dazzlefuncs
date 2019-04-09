9c7aea89c9048610cf67024c1aa063e4f67c0103
Nick Dimitrov <snoop088@gmail.com>
2019-04-09 18:12:50 +0300

    Additional logs

--- GenericTest/index.js
+++ GenericTest/index.js
@@ -4,30 +4,33 @@ module.exports = function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
     MongoClient.connect(process.env.CosmosDBConnectionString, (err, client) => {
         // assert.equal(null, err);
-        context.res = {
-            status: 200,
-            body: 'WTF'
-        }
-        context.done();
-        // const response = (client, context) => (status, body) => {
-        //     context.res = {
-        //         status: status,
-        //         body: body
-        //     };
-        //     client.close();
-        //     context.done();
+        // context.res = {
+        //     status: 200,
+        //     body: 'WTF'
         // }
-        // let send = response(client, context);
-        // if (err) send(500, err.message);
+        // context.done();
+        context.log('here');
+        const response = (client, context) => (status, body) => {
+            context.log('success result: ' + body);
+            context.res = {
+                status: status,
+                body: body
+            };
+            client.close();
+            context.done();
+        }
+        let send = response(client, context);
+        context.log('err: ' + err);
+        if (err) send(500, err.message);
 
-        // let db = client.db('dazzledb');
+        let db = client.db('dazzledb');
 
-        // db
-        //     .collection('items')
-        //     .find({})
-        //     .toArray((err, result) => {
-        //         if (err) send(500, err.message);
-        //         send(200, JSON.parse(JSON.stringify(result)));
-        //     });
+        db
+            .collection('items')
+            .find({})
+            .toArray((err, result) => {
+                if (err) send(500, err.message);
+                send(200, JSON.parse(JSON.stringify(result)));
+            });
     });
 };
